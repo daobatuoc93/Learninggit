7132c44ba5bd2d64b78af688b9b24b2190cc6c0c
Bá Tước Đào <daobatuoc93@gmail.com>
2020-06-27 20:20:43 +0700

    add for new branch

--- newfeature.py
+++ newfeature.py
@@ -1,3 +1,4 @@
 print("trying to get a neww features in new branch!")
 print("im friend and i fix something inhere21312312313123")
 print("yeah")
+print("add anything in more_brand to lean pull request!")
