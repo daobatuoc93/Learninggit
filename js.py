5333f6e1e28a71f1056183f03bb1f99823e6d862
daobatuoc93 <51743066+daobatuoc93@users.noreply.github.com>
2020-06-28 16:48:42 +0700

    Update js.py

--- js.py
+++ js.py
@@ -7,3 +7,4 @@ jsonstring = json.dumps(mydata)
 print (jsonstring)
 print("your friend have just changed something in this line,yet comfrom")
 print("i just add something")
+#need to add more thing.
